c84fa0584b79f81b98c5ff555bff04ed5c0a777d
ioio <io.manoliu@gmail.com>
2019-06-19 15:33:28 +0300

    Add test

--- server/tests/tests.js
+++ server/tests/tests.js
@@ -1,13 +1,13 @@
 const test = require('tape').test;
-const server = require('./server');
+const server = require('../server');
 
 test('A passing test', assert => {
   assert.pass('This test will pass.');
   assert.end();
 });
 test('Assertions with tape', assert => {
-  const expected = 3;
-  const actual = server.leaksList.length;
+  const expected = Object;
+  const actual = typeof server.dog;
 
   assert.equal(
     actual,
@@ -18,7 +18,3 @@ test('Assertions with tape', assert => {
   assert.end();
 });
 
-test('My first test', function(assert) {
-  assert.equal(1,14, 'Numbers 1 and 2 are the same');
-  assert.end();
-});
